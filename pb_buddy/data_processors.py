96e8fc5bab253f333f41f010670de32917d2dc39
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-09-05 13:24:41 -0600

    Updates for error handling
    Extract to package somewhat. Scraping working on category
    
    Clean up functions more. Start making parallelized
    
    PIA vpn functioning for large scrapes
    
    Incremental scrape working. Fix for Still For Sale Field implemented
    
    Add in incremental logic for updates, remomved, sold ads
    
    Tweak incremental adds. Ordering fixed of found urls
    
    Incremental scraping logic roughly done, checks on price, descrip. and title for updated ads
    
    Tweak saving parquet out of incremental scrape
    
    End to end scrape test run complete. Attempting to run on all categories
    
    Changelog generation function added in
    
    First full incremental scrape
    
    Re add logic for removing sold ads from write out. Update logging to subfolder
    
    Add in deduplication for cleaning up old data. Fix regex for price scrape
    
    Clean up. Add scrape action for testing
    
    Update action
    
    Fix path to env file in action file
    
    Fix env file name in action
    
    tweak action
    
    Fix to run action inside correct conda env. Attempt full run
    
    Fix for log writing
    
    Minor fix for checking against sold ads before adding. Test log directory
    
    Add in logs dir
    
    Setup for full scrape
    
    Add data for 2021-09-16

--- pb_buddy/data_processors.py
+++ pb_buddy/data_processors.py
@@ -1,17 +1,9 @@
-import os
-from io import BytesIO
-from typing import List, Optional
-
-import certifi
 import pandas as pd
-import pymongo
-from azure.storage.blob import BlobServiceClient
 
 
 def get_latest_by_scrape_dt(df: pd.DataFrame) -> pd.DataFrame:
     """Get latest rows for each 'url' sorted by 'datetime_scraped'.
-    For deduplicating ad data. Converts datetime_scraped to UTC, then Mountain
-    time zone for consistent comparisons.
+    For deduplicating ad data.
 
     Parameters
     ----------
@@ -24,237 +16,48 @@ def get_latest_by_scrape_dt(df: pd.DataFrame) -> pd.DataFrame:
         Dataframe deduplicated for each 'url' using 'datetime_scraped'
     """
     return (
-        df.assign(datetime_scraped=lambda x: pd.to_datetime(x.datetime_scraped, utc=True).dt.tz_convert("US/Mountain"))
+        df
+        .assign(datetime_scraped=lambda x: pd.to_datetime(x.datetime_scraped))
         .sort_values("datetime_scraped", ascending=False)
         .drop_duplicates(subset="url")
     )
 
 
-def get_dataset(category_num: int, data_type: str, region_code: Optional[int] = None) -> pd.DataFrame:
-    """Get active, sold or changed ad data for a given category number. Normalizes column
-    names into nicer forms. Requires env variable "COSMOS_CONN_STR" to
-    be set for MongoDB api on CosmosDB access.
-
-    Use `category_num` == -1 to access all data of a certain type.
+def get_category_base_data(category_num: int) -> pd.DataFrame:
+    """Get active ad data for a given category number.
 
     Parameters
     ----------
     category_num : int
         Category num to lookup.
-    data_type : str
-        Either "base","sold" or "changes" for active ads and sold ads respectively.
-    region_code : int, optional
-        Region code to get data for. Default of None returns for all
 
     Returns
     -------
     pd.DataFrame
         Dataframe of existing data for given category
     """
-    if data_type not in ["base", "sold", "changes"]:
-        raise ValueError("data_type should be one of 'base','sold','changes'")
-    if region_code not in [3, 5, None]:
-        raise ValueError("region_code should be one of 3,5 or None")
-
-    db = get_mongodb()
-
-    if data_type == "base":
-        database_mongo = db.base_data
-    elif data_type == "sold":
-        database_mongo = db.sold_data
-    elif data_type == "changes":
-        database_mongo = db.change_data
-    else:
-        raise ValueError("data_type must be one of 'base','sold','changes'")
-
-    query = {}
-    if category_num != -1:
-        query["category_num"] = category_num
-    if region_code is not None:
-        query["region_code"] = region_code
-
-    query_result = list(database_mongo.find(query))
-
-    if len(query_result) == 0:
-        df_out = pd.DataFrame({"url": []})
-    else:
-        df_out = pd.DataFrame(query_result)
-
-        if "datetime_scraped" in df_out.columns:
-            df_out = df_out.assign(
-                datetime_scraped=lambda x: pd.to_datetime(x.datetime_scraped, utc=True).dt.tz_convert("US/Mountain")
-            )
-    return df_out
-
-
-def write_dataset(category_df: pd.DataFrame, data_type: str):
-    """Write active/sold/changed ad data from a given DataFrame. Normalizes column
-    names into nicer forms (lower case, "_" separated). Uses unordered insert, ignores index errors.
-    Requires env variable "COSMOS_CONN_STR" to
-    be set for MongoDB api on CosmosDB access.
-
-    Parameters
-    ----------
-    category_df : pd.DataFrame
-        Data to be written to MongoDB collection.
-    data_type : str
-        Either "base","sold","changes" for active ads, sold ads, or changes
-
-    """
-    if data_type not in ["base", "sold", "changes"]:
-        raise ValueError("data")
-    db = get_mongodb()
-
-    if data_type == "base":
-        database_mongo = db.base_data
-    elif data_type == "sold":
-        database_mongo = db.sold_data
-    else:
-        database_mongo = db.change_data
-
-    # normalize column names
-    category_df.columns = [x.replace(":", "").replace(" ", "_").lower() for x in category_df.columns]
-
-    # Insert as many as possible, return without printing errors for now.
-    try:
-        database_mongo.insert_many(category_df.to_dict(orient="records"), ordered=False)
-    except pymongo.errors.BulkWriteError:
-        pass
-
-
-def update_base_data(category_df: pd.DataFrame, index_col: str, cols_to_update: List[str]):
-    """Update MongoDB collection for base ad data documents uniquely identified by `index_col` and update fields
-    with values in `cols_to_update` from `category_df`. Requires env variable "COSMOS_CONN_STR" to
-    be set for MongoDB api on CosmosDB access.
-
-    Parameters
-    ----------
-    category_df : pd.DataFrame
-        Dataframe of data containing `index_col` and `cols_to_update` data to update MongoDB
-        documents with.
-    index_col : str
-        Column to match each row of `category_df` with a MongoDB document. Must match a field in MongoDB documents.
-    cols_to_update : List[str]
-        Columns to use for updating data in MongoDB documents. Must all be existing fields in MongoDB documents.
-    """
-    if len(set(category_df.columns).intersection([index_col] + cols_to_update)) != len([index_col] + cols_to_update):
-        raise ValueError("index_col and cols_to_update must all be present in category_df")
-
-    db = get_mongodb()
-    database_mongo = db.base_data
-
-    for row in category_df.itertuples():
-        updates_dict = {x: getattr(row, x) for x in cols_to_update}
-        database_mongo.update_one({index_col: getattr(row, index_col)}, {"$set": updates_dict})
-
-
-def remove_from_base_data(removal_df: pd.DataFrame, index_col: str):
-    """Remove documents from MongoDB `base_data` database based on `index_col`. Requires env variable "COSMOS_CONN_STR" to
-    be set for MongoDB api on CosmosDB access.
-
-    Parameters
-    ----------
-    removal_df : pd.DataFrame
-        Dataframe that contains `index_col` of all documents to remove
-    index_col : str
-        Column to match on in documents fields.
-    """
-    if index_col not in removal_df.columns:
-        raise ValueError("index_col must be in removal_df.columns")
-
-    db = get_mongodb()
-    database_mongo = db.base_data
-
-    for val in removal_df[index_col]:
-        database_mongo.delete_one({index_col: val})
-
-
-def get_mongodb():
-    """
-    Return a `pymongo` database object to work with pb-buddy data
-    Requires env variable "COSMOS_CONN_STR" to
-    be set for MongoDB api on CosmosDB access.
-    """
-    # mongo_user = os.environ['MONGO_USER']
-    # mongo_pass = os.environ['MONGO_PASS']
-    # Replace the uri string with your MongoDB deployment's connection string.
-    conn_str = os.environ["COSMOS_CONN_STR"]
-    # set a 5-second connection timeout
-    client = pymongo.MongoClient(
-        conn_str,
-        tlsCAFile=certifi.where(),
-        serverSelectionTimeoutMS=200000,
-        connectTimeoutMS=300000,
-        socketTimeoutMS=None,
-        socketKeepAlive=True,
-        connect=False,
-        maxPoolsize=1,
+    return (
+        pd.read_parquet(
+            f"data/base_data/category_{category_num}_ad_data.parquet.gzip",
+        )
     )
-    db = client["pb-buddy"]
-    return db
 
 
-def stream_parquet_to_blob(df: pd.DataFrame, blob_name: str, blob_container: str):
-    """Stream pandas DataFrame to parquet.gzip blob in Azure Blob storage.
-    Uses `pyarrow` and requires env variable to be set:
-    AZURE_STORAGE_CONN_STR=Connection string to authenticate with.
+def get_category_sold_data(category_num: int) -> pd.DataFrame:
+    """Get sold ad data for the category.
 
     Parameters
     ----------
-    df : pd.DataFrame
-        Pandas DataFrame to be uploaded to Azure Blob
-    blob_name : str
-        File name to upload as in container
-    blob_container : str
-        Container to upload to
-    """
-    # Create a blob client using the local account credentials
-    blob_service_client = BlobServiceClient.from_connection_string(os.environ["AZURE_STORAGE_CONN_STR"])
-
-    # Create a unique name for the container
-    container_name = blob_container
-
-    # Create a blob client using the container client
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-
-    parquet_file = BytesIO()
-    df.to_parquet(parquet_file, engine="pyarrow", compression="gzip")
-    parquet_file.seek(0)  # change the stream position back to the beginning after writing
-
-    blob_client.upload_blob(data=parquet_file)
-
-
-def stream_parquet_to_dataframe(blob_name: str, blob_container: str) -> pd.DataFrame:
-    """Stream pandas DataFrame as parquet.gzip blob from Azure Blob storage.
-    Uses `pyarrow` and requires env variable to be set:
-    AZURE_STORAGE_CONN_STR=Connection string to authenticate with.
-
-    Parameters
-    ----------
-    blob_name : str
-        File name to upload as in container
-    blob_container : str
-        Container to upload to
+    category_num : int
+        Category number
 
     Returns
     -------
     pd.DataFrame
-        DataFrame when blob is downloaded
+        Sold ad data
     """
-    # Create a blob client using the local account credentials
-    blob_service_client = BlobServiceClient.from_connection_string(os.environ["AZURE_STORAGE_CONN_STR"])
-
-    # Create a unique name for the container
-    container_name = blob_container
-
-    # Create a blob client using the container client
-    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-
-    stream_download = blob_client.download_blob()
-    stream = BytesIO()
-    stream_download.readinto(stream)
-
-    df = pd.read_parquet(stream, engine="pyarrow")
-
-    return df
-    return df
+    return (
+        pd.read_parquet(
+            f"data/sold_ads/category_{category_num}_sold_ad_data.parquet.gzip",
+        )
+    )
--- pb_buddy/scraper.py
+++ pb_buddy/scraper.py
@@ -1,169 +1,48 @@
-import re
-import time
-from enum import Enum
-from typing import Callable
-
-import pandas as pd
+import requests
+from typing import List
 from bs4 import BeautifulSoup
-from playwright.sync_api import sync_playwright
-from tenacity import retry, stop_after_attempt, wait_fixed
+import re
+import pandas as pd
+import time
 
 
-class PlaywrightScraper:
-    cookies = []
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
-    }
-
-    def __init__(self, headless: bool = True):
-        self.page = None
-        self.browser = None
-        self.headless = headless
-        self.initialized = True
-
-    def start_browser(self):
-        if self.browser is None:
-            self._playwright = sync_playwright().start()
-            self.browser = self._playwright.chromium.launch(headless=self.headless)
-            self.context = self.browser.new_context()
-            self.context.set_extra_http_headers(self.headers)
-            self.page = self.context.new_page()
-
-    def close_browser(self):
-        if self.browser:
-            self.browser.close()
-            self.browser = None
-        if hasattr(self, "_playwright"):
-            self._playwright.stop()
-
-    def set_cookies(self, cookies: list):
-        if self.context is None:
-            raise Exception("Browser context is not started. Call start_browser() first.")
-        self.context.add_cookies(cookies)
-
-    def get_page_content(self, url: str):
-        "Use playwright to avoid detection for getting a page's content"
-        if self.page is None:
-            self.start_browser()
-        self.page.goto(url)
-        return self.page.content()
-
-    def process_urls(self, urls: list, callable_func: Callable):
-        """Process a list of URLs with a callable function.
-
-        Parameters
-        ----------
-        urls : list
-            URLs to visit and get page contents for
-        callable_func : Callable
-            callable function to process the page content.
-        """
-        with sync_playwright() as playwright:
-            browser = playwright.chromium.launch(headless=self.headless)
-            context = browser.new_context()
-            context.set_extra_http_headers(self.headers)
-            page = context.new_page()
-            results = []
-            for url in urls:
-                page.goto(url)
-                page_content = page.content()
-                results.append(callable_func(page_content))
-
-            context.close()
-            browser.close()
-            return results
-
-
-def get_category_list(playwright_scraper: PlaywrightScraper) -> dict:
-    """
-    Get the mapping of category name to category number
-    for all categories from https://www.pinkbike.com/buysell/
-    """
-    page_content = playwright_scraper.get_page_content("https://www.pinkbike.com/buysell/")
-
-    soup = BeautifulSoup(page_content, features="html.parser")
-
-    category_dict = {}
-    for link in soup.find_all("a"):
-        if link.get("href") is None:
-            continue
-        category_num_match = re.match(".*category=([0-9]{1,20})", link.get("href"))
-
-        if category_num_match is not None:
-            category_text = link.text
-            category_dict[category_text] = int(category_num_match.group(1))
-
-    return category_dict
-
-
-@retry(stop=stop_after_attempt(5), wait=wait_fixed(2))
-def request_ad(url: str, playwright_scraper: PlaywrightScraper, delay_s: int = 1) -> str:
-    """Request a URL and return the page HTML content using Playwright.
+def get_buysell_ads(url: str, delay_s: int = 1) -> List[str]:
+    """Grab all buysell URL's from a page of Pinkbike's buysell results
 
     Parameters
     ----------
     url : str
-        URL to request
+        URL to a buysell results page to extract all ad links from
     delay_s : int
         Time in seconds to delay before returning.
 
     Returns
     -------
-    str
-        Page content as a string
+    List[str]
+        List of URL's to the buysell ads on that page
     """
-    page_content = playwright_scraper.get_page_content(url)
+    search_results = requests.get(url)
+    if search_results.status_code > 200:
+        raise requests.exceptions.RequestException("Buysell url status error")
+    soup = BeautifulSoup(search_results.content, features="html.parser")
+    buysell_ads = []
+    for link in soup.find_all("a"):
+        if re.match("https://www.pinkbike.com/buysell/[0-9]{7}", link.get("href")):
+            buysell_ads.append(link.get("href"))
 
     time.sleep(delay_s)
-    return page_content
+    # Return de duplicated, order maintained
+    return list(dict.fromkeys(buysell_ads))
 
 
-def get_buysell_ads(search_results: str) -> dict:
-    """Grab all buysell URL's from a page of Pinkbike's buysell results
-
-    Parameters
-    ----------
-    search_results : str
-        HTML content of the search results page
-
-    Returns
-    -------
-    dict
-        Mapping of ad URL: "boosted" or "not boosted"
-    """
-    soup = BeautifulSoup(search_results, features="html.parser")
-    buysell_ad_links = []
-
-    # Get all buysell ad links
-    for link in soup.find_all("a"):
-        if link.get("href") is None:
-            continue
-        if re.match("https://www.pinkbike.com/buysell/[0-9]{6,7}", link.get("href")):
-            buysell_ad_links.append(link.get("href"))
-
-    # De-duplicate, order maintained
-    buysell_ad_links = list(dict.fromkeys(buysell_ad_links))
-
-    # Get if each ad is boosted or not
-    buysell_ad_status = {}
-    divs = soup.find_all("div", class_=["bsitem boosted", "bsitem"])
-    for i, div in enumerate(divs):
-        if "boosted" in div.get("class"):
-            buysell_ad_status[buysell_ad_links[i]] = "boosted"
-        else:
-            buysell_ad_status[buysell_ad_links[i]] = "not boosted"
-
-    return buysell_ad_status
-
-
-def get_total_pages(category_num: int, playwright_scraper: PlaywrightScraper, region: int = 3) -> int:
+def get_total_pages(category_num: str, region: int = 3) -> int:
     """Get the total number of pages in the Pinkbike buysell results
     for a given category number
 
     Parameters
     ----------
     category_num : int
-        Category number to grab total number of pages of ads for
+        Category number to grab total number of paes of ads for
     region : int
         Region to get results for. Default of 3 is North America.
 
@@ -173,17 +52,12 @@ def get_total_pages(category_num: int, playwright_scraper: PlaywrightScraper, re
         Total number of pages possible to click through to.
     """
     # Get total number of pages-----------------------------------------------
-    search_results = request_ad(
-        f"https://www.pinkbike.com/buysell/list/?region={region}&page=1&category={category_num}",
-        playwright_scraper=playwright_scraper,
-        delay_s=0,
-    )
+    base_url = f"https://www.pinkbike.com/buysell/list/?region={region}&page=1&category={category_num}"
+    search_results = requests.get(base_url).content
     soup = BeautifulSoup(search_results, features="html.parser")
 
     largest_page_num = 0
     for link in soup.find_all("a"):
-        if link.get("href") is None:
-            continue
         page_num = re.match(".*page=([0-9]{1,20})", link.get("href"))
         # If page number is found, convert to int from only match and compare
         if page_num is not None and int(page_num.group(1)) > largest_page_num:
@@ -191,20 +65,8 @@ def get_total_pages(category_num: int, playwright_scraper: PlaywrightScraper, re
     return largest_page_num
 
 
-class AdType(Enum):
-    PINKBIKE = "pinkbike"
-    BUYCYCLE = "buycycle"
-    OTHER = "other"
-
-
-def parse_buysell_ad(
-    buysell_url: str,
-    delay_s: int,
-    region_code: int,
-    playwright_scraper: PlaywrightScraper,
-    ad_type: AdType = AdType.PINKBIKE,
-) -> dict:
-    """Takes a buysell URL and extracts all attributes listed for product.
+def parse_buysell_ad(buysell_url: str, delay_s: int) -> dict:
+    """Takes a Pinkbike buysell URL and extracts all attributes listed for product.
 
     Parameters
     ----------
@@ -212,152 +74,69 @@ def parse_buysell_ad(
         URL to the buysell ad
     delay_s : int
         Number of seconds to sleep before returning
-    region_code : int
-        Region code for the ad
-    playwright_scraper : PlaywrightScraper
-        PlaywrightScraper object to store playwright browser context for requests
-    ad_type : AdType
-        Type of the ad, default is AdType.PINKBIKE
 
     Returns
     -------
     dict
-        Dictionary with all ad data, plus URL and datetime scraped.
+        dictionary with all ad data, plus URL and date scraped.
     """
-    page_request = request_ad(buysell_url, delay_s=delay_s, playwright_scraper=playwright_scraper)
-
-    soup = BeautifulSoup(page_request, features="html.parser")
-    if ad_type == AdType.PINKBIKE:
-        results = parse_buysell_pinkbike_ad(soup)
-    elif ad_type == AdType.BUYCYCLE:
-        results = parse_buysell_buycycle_ad(soup)
-    else:
-        # results = parse_buysell_other_ad(soup)
-        raise NotImplementedError(f"Ad type {ad_type} not implemented")
-
-    if results:
-        results["region_code"] = region_code
-        results["url"] = buysell_url
-        results["datetime_scraped"] = str(pd.Timestamp.today(tz="US/Mountain"))
-    return results
-
-
-def parse_buysell_pinkbike_ad(page_content: BeautifulSoup) -> dict:
-    """Takes a Pinkbike buysell URL and extracts all attributes listed for a product.
-
-    Parameters
-    ----------
-    page_content : BeautifulSoup
-        BeautifulSoup object representing the HTML content of the page
-
-    Returns
-    -------
-    dict
-        Dictionary with all ad data
-    """
-    soup = page_content
+    page_request = requests.get(buysell_url)
+    if page_request.status_code > 200:
+        # raise requests.exceptions.RequestException("Error requesting Ad")
+        return {}
+    soup = BeautifulSoup(page_request.content, features="html.parser")
 
     data_dict = {}
-
-    # Combine all relevant divs into a single find_all call
-    details_divs = soup.select("div.buysell-container.details")
-    price_divs = soup.select("div.buysell-container.buysell-price")
-    description_divs = soup.select("div.buysell-container.description")
-    title_divs = soup.select("h1.buysell-title")
-    location_spans = soup.select("span.f11")
-    restriction_divs = soup.select("div.buysell-container")
-
     # Get data about product
-    for details in details_divs:
+    for details in soup.find_all("div", class_="buysell-container details"):
         for tag in details.find_all("b"):
+            # Still For Sale has a filler element we need to skip to get text
             if "Still For Sale" in tag.text:
-                data_dict[tag.text] = tag.next_sibling.next_sibling.get_text().strip()
+                data_dict[tag.text] = tag.next_sibling.next_sibling.text
             else:
-                data_dict[tag.text] = str(tag.next_sibling).strip()
+                data_dict[tag.text] = tag.next_sibling.text
 
-    # Precompile the regular expression for efficiency
-    price_currency_re = re.compile(r"([\d,]+)\s*([A-Za-z]{1,3})")
+    # Get price
+    for pricing in soup.find_all("div", class_="buysell-container buysell-price"):
 
-    # Get price and currency
-    for pricing in price_divs:
-        match = price_currency_re.search(pricing.text)
-        if match:
-            price_str, currency = match.groups()
-            data_dict["price"] = float(price_str.replace(",", ""))
-            data_dict["currency"] = currency
+        # Grab price and currency. In case of issue, store None and handle downstream
+        price_search = re.search("([\d,]+)", pricing.text)
+        if price_search is not None:
+            data_dict["price"] = float(price_search.group(0).replace(",",""))
         else:
             data_dict["price"] = None
+
+        currency_search = re.search("([A-Za-z]{1,3})", pricing.text)
+        if currency_search is not None:
+            data_dict["currency"] = currency_search.group(0)
+        else:
             data_dict["currency"] = None
 
     # Get description
-    for description in description_divs:
+    for description in soup.find_all("div", class_="buysell-container description"):
         data_dict["description"] = description.text
 
     # Get ad title
-    for title in title_divs:
+    for title in soup.find_all("h1", class_="buysell-title"):
         data_dict["ad_title"] = title.text
 
     # Get user city, country
-    for sp in location_spans:
+    for sp in soup.find_all("span", class_="f11"):
         if "Member" in sp.text:
             location = sp.text.split("\n")[2].strip()
             data_dict["location"] = location
 
-    # Get restrictions on shipping
-    for sp in restriction_divs:
-        if "Restrictions" in sp.text:
-            data_dict["restrictions"] = sp.text.split(":")[-1]
+    # Add scrape datetime
+    data_dict["datetime_scraped"] = str(pd.Timestamp.today())
+    data_dict["url"] = buysell_url
 
-    # Clean non standard whitespace, fix key names:
+    # Clean whitespace a little:
+    pattern = re.compile(r"\s+")
     data_dict = {
-        k.replace(":", "").replace(" ", "_").lower(): " ".join(v.split()).strip() if isinstance(v, str) else v
+        k: re.sub(pattern, " ", v) if type(v) == str else v
         for k, v in data_dict.items()
     }
 
-    return data_dict
-
-
-def parse_buysell_buycycle_ad(page_content: BeautifulSoup) -> dict:
-    """Takes a Buycycle buysell URL and extracts all attributes listed for a product.
-
-    Parameters
-    ----------
-    page_content : BeautifulSoup
-        BeautifulSoup object representing the HTML content of the page
-
-    Returns
-    -------
-    dict
-        Dictionary with all ad data
-    """
-    soup = page_content
-
-    data_dict = {}
-
-    ad_title = soup.find("h1", class_="text-3xl font-500 content-primary mb-1").text
-    ad_title = " ".join(ad_title.split())
-    price_text = soup.find("p", class_="text-3xl font-500 mb-0 content-sale").text
-    price = price_text.split()[0].replace(".", "")
-    currency = price_text.split()[1]
-    # No post date found on site, so using current date
-    original_post_date = str(pd.Timestamp.today(tz="US/Mountain"))
-    country = soup.find("p", class_="text-sm content-tertiary mb-0").text.strip()
-    details_divs = soup.find_all("div", class_="pdp-modal-content")
-    for div in details_divs:
-        if "Condition & details" in div.text:
-            # # Remove occurences of multiple whitespace in a row
-            # # with a single occurence of respective whitespace
-            details = re.sub(r" +", " ", div.text)
-            details = re.sub(r"\n +", r"\n", details)
-            details = re.sub(r"\n+", r"\n", details)
-            # Remove the newline before each colon, and then add a newline after each colon
-            details = re.sub(r"\n:", ":", details)
-            details = re.sub(r":", ":\n", details)
-    data_dict["ad_title"] = ad_title
-    data_dict["price"] = price
-    data_dict["currency"] = currency
-    data_dict["original_post_date"] = original_post_date
-    data_dict["location"] = country
-    data_dict["description"] = details
+    time.sleep(delay_s)
 
     return data_dict
