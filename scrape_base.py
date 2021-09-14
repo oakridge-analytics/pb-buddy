019b9fb89969cbfb09c9f318dafdc6f2d09bb801
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-09-14 13:39:01 -0600

    Add in incremental logic for updates, remomved, sold ads

--- scrape_base.py
+++ scrape_base.py
@@ -18,13 +18,17 @@ import pb_buddy.utils as ut
 
 # %%
 # Settings -----------------------------------------------------------------
-categories_to_scrape = range(100, 101)
+categories_to_scrape = range(1, 101 + 1)
 num_jobs = 8  # Curently only for initial link grab
-delay_s = 0.1
+delay_s = 0.0
+use_vpn = False
+
 
 # Setup PIA vpn object for manipulation. Make sure to connect
 vpn = PiaVpn()
 vpn.connect(verbose=False, timeout=20)
+vpn.set_region("random")
+time.sleep(15)
 
 # Setup output folder
 folder_name = str(pd.Timestamp("today").date())
@@ -35,16 +39,14 @@ os.makedirs(output_dir, exist_ok=True)
 with open("category_dict.json", "r") as fh:
     cat_dict = json.load(fh)
 
-vpn.set_region("random")
-time.sleep(10)
-
 # Iterate through all categories in random order, prevent noticeable patterns?
 num_categories_scraped = 0
 for category_to_scrape in np.random.choice(
     categories_to_scrape, size=len(categories_to_scrape), replace=False
 ):
     print(
-        f"*************Starting Category {[x for x,v in cat_dict.items() if v== category_to_scrape]} - Number: {category_to_scrape}. {num_categories_scraped} Categories Scraped So Far"
+        f"** ***********Starting Category {[x for x,v in cat_dict.items() if v == category_to_scrape]} \
+        - Number: {category_to_scrape}. {num_categories_scraped} Categories Scraped So Far"
     )
 
     # Get existing open ads and stats of last scrape
@@ -52,7 +54,7 @@ for category_to_scrape in np.random.choice(
         "data",
         "base_data",
         f"category_{category_to_scrape}_ad_data.parquet.gzip"))
-    last_scrape_dt = pd.to_datetime(base_data.datetime_scraped.max())
+    last_scrape_dt = pd.to_datetime(base_data.datetime_scraped).max()
 
     # Get total number of pages-----------------------------------------------
     total_page_count = scraper.get_total_pages(category_to_scrape)
@@ -71,11 +73,10 @@ for category_to_scrape in np.random.choice(
     )
     ad_urls = ut.flatten(ad_urls)
     # Change region and wait for reconnect before continuing
-    vpn.set_region("random")
-    time.sleep(10)
+    # vpn.set_region("random")
+    # time.sleep(10)
 
     # Get new ad data ---------------------------------------------------------
-    ad_data = pd.DataFrame()
     intermediate_ad_data = []
     print(f"Extracting ad data from {len(ad_urls)} ads")
 
@@ -83,37 +84,56 @@ for category_to_scrape in np.random.choice(
     # Only add new ads
     for url in ad_urls:
         single_ad_data = scraper.parse_buysell_ad(url, delay_s=0)
-        if pd.to_datetime(single_ad_data["Last Repost Date:"]) > \
-                last_scrape_dt:
+        display_string = f"\r Checking ad from {single_ad_data['Last Repost Date:']} vs. last scrape date {last_scrape_dt}"
+        print(display_string, sep=' ', end='', flush=True)
+        if pd.to_datetime(single_ad_data["Last Repost Date:"]).date() >= \
+                last_scrape_dt.date():
             intermediate_ad_data.append(single_ad_data)
         else:
+            print("")
+            print(
+                f"Repost datetime of {single_ad_data['Last Repost Date:']} before last scrape on {last_scrape_dt}")
             break
 
-    print(f" Adding {len(intermediate_ad_data)} new ads to base data")
+    # New ads where not in existing urls
+    if len(intermediate_ad_data) == 0:
+        continue
+    else:
+        recently_added_ads = pd.DataFrame(intermediate_ad_data)
+        new_ads = recently_added_ads.loc[~recently_added_ads.url.isin(
+            base_data.url),:]
+        updated_ads = recently_added_ads.loc[recently_added_ads.url.isin(
+            base_data.url),:]
 
-    ad_data = pd.concat(
-        [base_data, pd.DataFrame(intermediate_ad_data)], axis=0)
-    ad_data.to_csv(
-        output_dir.joinpath(f"category_{category_to_scrape}_ad_data.csv"),
-        index=False,
-    )
+        ad_data = pd.concat(
+            [base_data.loc[~base_data.url.isin(updated_ads)],
+             updated_ads,
+             new_ads], axis=0)
+
+    print(f" Adding {len(new_ads)} new ads to base data")
+    print(f" Updating {len(updated_ads)} ads")
 
     # Get sold ad data ------------------------------------------------
-    sold_urls = base_data.loc[~ base_data.url.isin(ad_urls), "url"]
+    potentially_sold_urls = base_data.loc[~ base_data.url.isin(ad_urls), "url"]
 
     # Rescrape sold ads to make sure, check for "SOLD"
     sold_ad_data = pd.DataFrame()
     intermediate_ad_data = []
-    print(f"Extracting ad data from {len(sold_urls)} potentially sold ads")
+    print(
+        f"Extracting ad data from {len(potentially_sold_urls)} potentially sold ads")
 
     # Do sequentially and check datetime of last scrape
     # Only add new ads
-    for url in sold_urls:
+    urls_to_remove = []
+    for url in tqdm(potentially_sold_urls):
         single_ad_data = scraper.parse_buysell_ad(url, delay_s=0)
         if single_ad_data and "sold" in single_ad_data["Still For Sale:"].lower():
             intermediate_ad_data.append(single_ad_data)
+        else:
+            urls_to_remove.append(url)
 
-    print(f"Found {len(intermediate_ad_data)} sold ads")
+    print(
+        f"Found {len(intermediate_ad_data)} sold ads, {len(urls_to_remove)} removed ads")
 
     sold_ad_data = pd.concat(
         [sold_ad_data, pd.DataFrame(intermediate_ad_data)], axis=0)
@@ -121,13 +141,17 @@ for category_to_scrape in np.random.choice(
         output_dir.joinpath(f"category_{category_to_scrape}_sold_ad_data.csv"),
         index=False,
     )
-    # check for all ads that dropped out
-
-    f"*************Finished Category {[x for x,v in cat_dict.items() if v== category_to_scrape]}"
+    # remove ads that didn't sell but shouldn't be in anymore ---------------
+    ad_data.loc[~ ad_data.url.isin(urls_to_remove), :].to_csv(
+        output_dir.joinpath(f"category_{category_to_scrape}_ad_data.csv"),
+        index=False,
+    )
+    print(
+        f"*************Finished Category {[x for x,v in cat_dict.items() if v== category_to_scrape]}")
 
     num_categories_scraped += 1
     # Change region and wait for reconnect before continuing
-    vpn.set_region("random")
-    time.sleep(10)
+    # vpn.set_region("random")
+    # time.sleep(10)
 
 # %%
