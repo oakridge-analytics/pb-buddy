0e205509bb4e18c0aa3f87c66f6398d8e2694068
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-09-14 18:44:16 -0600

    Incremental scraping logic roughly done, checks on price, descrip. and title for updated ads

--- scrape_base.py
+++ scrape_base.py
@@ -18,7 +18,7 @@ import pb_buddy.utils as ut
 
 # %%
 # Settings -----------------------------------------------------------------
-categories_to_scrape = range(2, 3)
+categories_to_scrape = range(50, 60)
 num_jobs = os.cpu_count()  # Curently only for initial link grab
 delay_s = 0.0
 use_vpn = False
@@ -94,14 +94,14 @@ for category_to_scrape in np.random.choice(
         single_ad_data = scraper.parse_buysell_ad(url, delay_s=0)
         display_string = f"\r Checking ad from {single_ad_data['Last Repost Date:'].strip()} vs. last scrape date {last_scrape_dt}"
         print(display_string, sep=' ', end='', flush=True)
-        if pd.to_datetime(single_ad_data["Last Repost Date:"]).date() >= \
-                last_scrape_dt.date():
-            intermediate_ad_data.append(single_ad_data)
-        else:
-            print("")
-            print(
-                f"Repost datetime of {single_ad_data['Last Repost Date:'].strip()} before last scrape on {last_scrape_dt}")
-            break
+
+        if single_ad_data != {}:
+            if pd.to_datetime(single_ad_data["Last Repost Date:"]).date() >= \
+                    last_scrape_dt.date():
+                intermediate_ad_data.append(single_ad_data)
+            else:
+                print("")
+                break
 
     # New ads where not in existing urls
     if len(intermediate_ad_data) == 0:
@@ -110,8 +110,23 @@ for category_to_scrape in np.random.choice(
         recently_added_ads = pd.DataFrame(intermediate_ad_data)
         new_ads = recently_added_ads.loc[~recently_added_ads.url.isin(
             base_data.url),:]
-        updated_ads = recently_added_ads.loc[recently_added_ads.url.isin(
-            base_data.url),:]
+
+        # Get ads that might have an update. check price, description and title
+        # for change.
+        cols_to_check = ["price", "description", "ad_title"]
+        updated_ads = (recently_added_ads
+                       .loc[recently_added_ads.url.isin(base_data.url),:]
+                       .sort_values("url")
+                       .reset_index(drop=True)
+                       )
+        previous_versions = (base_data
+                             .loc[base_data.url.isin(updated_ads.url)]
+                             .sort_values("url")
+                             .reset_index(drop=True)
+                             )
+        unchanged_mask = updated_ads[cols_to_check].eq(
+            previous_versions[cols_to_check]).all(1)
+        updated_ads = updated_ads[~unchanged_mask]
 
         ad_data = pd.concat(
             [base_data.loc[~base_data.url.isin(updated_ads)],
@@ -125,7 +140,16 @@ for category_to_scrape in np.random.choice(
     potentially_sold_urls = base_data.loc[~ base_data.url.isin(ad_urls), "url"]
 
     # Rescrape sold ads to make sure, check for "SOLD"
-    sold_ad_data = pd.DataFrame()
+    sold_data_path = os.path.join(
+        "data",
+        "sold_ads",
+        f"category_{category_to_scrape}_sold_ad_data.parquet.gzip"
+    )
+    if os.path.isfile(sold_data_path):
+        sold_data = pd.read_parquet(sold_data_path)
+    else:
+        sold_ad_data = pd.DataFrame()
+
     intermediate_ad_data = []
     print(
         f"Extracting ad data from {len(potentially_sold_urls)} potentially sold ads")
@@ -145,14 +169,17 @@ for category_to_scrape in np.random.choice(
 
     sold_ad_data = pd.concat(
         [sold_ad_data, pd.DataFrame(intermediate_ad_data)], axis=0)
-    sold_ad_data.to_csv(
-        output_dir.joinpath(f"category_{category_to_scrape}_sold_ad_data.csv"),
+    sold_ad_data.to_parquet(
+        sold_data_path,
+        compression="gzip",
         index=False,
     )
-    # remove ads that didn't sell but shouldn't be in anymore ---------------
-    ad_data.loc[~ ad_data.url.isin(urls_to_remove), :].to_csv(
-        output_dir.joinpath(f"category_{category_to_scrape}_ad_data.csv"),
+
+    # remove ads that didn't sell and shouldn't be in anymore ---------------
+    ad_data.loc[~ ad_data.url.isin(urls_to_remove), :].to_parquet(
+        base_data_path,
         index=False,
+        compression="gzip"
     )
     print(
         f"*************Finished Category {[x for x,v in cat_dict.items() if v== category_to_scrape]}")
