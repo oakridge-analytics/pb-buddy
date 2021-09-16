d8a25c549246dc93441d3705a091df1e29f361a8
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-09-16 16:56:07 -0600

    Update for more frequent scrape
    Setup alerts. Add tz info to datetime scraping
    
    Email alerts script working
    
    Try running email job
    
    Rollback for nightly scrape
    
    Add data for 2021-09-17

--- pb_buddy/emailer.py
+++ pb_buddy/emailer.py
@@ -1,24 +1,19 @@
 # %%
 import smtplib
-from dotenv import load_dotenv
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from email.mime.application import MIMEApplication
 import pandas as pd
 import os
-
-load_dotenv()
 
 
 def email_df(df: pd.DataFrame, email_to: str, email_subject: str):
     """Email a pandas Dataframe as an HTML table to a specific
-    recipient using Hotmail(Outlook) SMTP server through Twilio SendGrid relay. Relies on environment variables being set to handle
-    sender's email and username/password for Twilio API.
+    recipient using Hotmail(Outlook) SMTP server. Relies on environment variables being set to handle
+    sender's email and password.
 
     Required env variables:
-    - `TWILIO_USER` - sender's user name
-    - `TWILIO_PASS` - sender's password
-    - `HOTMAIL_ADDRESS` - sender's email address
+    - `HOTMAIL_USER` - sender's email address
+    - `HOTMAIL_PASS` - sender's password
 
     Parameters
     ----------
@@ -30,88 +25,40 @@ def email_df(df: pd.DataFrame, email_to: str, email_subject: str):
         Subject for email
     """
     # Get sender's user info
-    env_vars = ["TWILIO_USER", "TWILIO_PASS", "HOTMAIL_ADDRESS"]
+    env_vars = ["HOTMAIL_USER","HOTMAIL_PASS"]
     for v in env_vars:
         try:
             os.environ[v]
         except KeyError:
             raise KeyError(
-                "Ensure TWILIO_USER,TWILIO_PASS,HOTMAIL_ADDRESS environ variables are set prior to running!"
-            )
+                "Ensure HOTMAIL_USER,HOTMAIL_PASS environ variables are set prior to running!")
 
     msg = MIMEMultipart()
-    msg["From"] = os.environ["HOTMAIL_ADDRESS"]
-    msg["To"] = email_to
-    msg["Subject"] = email_subject
+    msg['From'] = os.environ["HOTMAIL_USER"]
+    msg['To'] = email_to
+    msg['Subject'] = email_subject
 
+    # Build, attach table
     html = f"""
-    <table border='2' cellpadding='0' cellspacing='0' width='100%' bgcolor='#e37b46' style='background: rgb(227,123,70); background: linear-gradient(315deg, rgba(227,123,70,1) 3%, rgba(198,51,92,1) 44%, rgba(86,50,139,1) 85%);'>
+    <html>
+    <head></head>
+    <body>
         {df.to_html()}
-    </table>
+    </body>
+    </html>
     """
 
-    part1 = MIMEText(html, "html")
+    part1 = MIMEText(html, 'html')
     msg.attach(part1)
 
-    s = smtplib.SMTP("smtp.sendgrid.net", 587)
+    s = smtplib.SMTP("smtp.live.com",587)
     # Hostname to send for this command defaults to the fully qualified domain name of the local host.
     s.ehlo()
     s.starttls()  # Puts connection to SMTP server in TLS mode
     s.ehlo()
-    s.login(os.environ["TWILIO_USER"], os.environ["TWILIO_PASS"])
+    s.login(msg["From"],os.environ["HOTMAIL_PASS"])
     s.sendmail(msg["From"], msg["To"], msg.as_string())
 
     s.quit()
 
-
-def email_html_report(report_path: str, email_subject: str):
-    """
-    Email an HTML report as an attachment to  myself
-    using Hotmail(Outlook) SMTP server through Twilio SendGrid relay.
-    Relies on environment variables being set to handle
-    sender's email and username/password for Twilio API.
-
-    Required env variables:
-    - `TWILIO_USER` - sender's user name
-    - `TWILIO_PASS` - sender's password
-    - `HOTMAIL_ADDRESS` - sender's email address
-    - `ADMIN_EMAIL` - email address to send the report to
-
-    Parameters
-    ----------
-    report_path : str
-        HTML report file path to attach to email
-    email_to : str
-        Recipient's email
-    email_subject : str
-        Subject for email
-    """
-    # Get sender's user info
-    env_vars = ["TWILIO_USER", "TWILIO_PASS", "HOTMAIL_ADDRESS"]
-    for v in env_vars:
-        try:
-            os.environ[v]
-        except KeyError:
-            raise KeyError(
-                "Ensure TWILIO_USER,TWILIO_PASS,HOTMAIL_ADDRESS environ variables are set prior to running!"
-            )
-
-    msg = MIMEMultipart()
-    msg["From"] = os.environ["HOTMAIL_ADDRESS"]
-    msg["To"] = os.environ["ADMIN_EMAIL"]
-    msg["Subject"] = email_subject
-
-    report = MIMEApplication(open(report_path, "r").read())
-    report.add_header('Content-Disposition', 'attachment', filename="scrape_report.html")
-    msg.attach(report)
-
-    s = smtplib.SMTP("smtp.sendgrid.net", 587)
-    # Hostname to send for this command defaults to the fully qualified domain name of the local host.
-    s.ehlo()
-    s.starttls()  # Puts connection to SMTP server in TLS mode
-    s.ehlo()
-    s.login(os.environ["TWILIO_USER"], os.environ["TWILIO_PASS"])
-    s.sendmail(msg["From"], msg["To"], msg.as_string())
-
-    s.quit()
-    
+    # %%
