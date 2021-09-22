440703d83505517a3e1bbeb6167d7798e401a31a
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-09-22 00:01:03 -0600

    Add loader script for mongo
    Start refactor for Mongo data access
    
    Move query of data for scrape outside loop for reuse. Add change data to load
    
    Start adding a write method for mongo
    
    Refactor for MongoDB storage working
    
    Reload recent parquet data
    
    Changes for running on Mongo backend on scraping
    
    Scraping script updated for Mongo backend
    
    Add in checks against all documents for change of category
    
    Continue tweaks for change of category comparison logic
    
    Hopefully final tweak to get scraping working with Mongo backend
    
    Add rough Azure function for testing
    
    First work for using CosmosDB started
    
    Data loading working with CosmosDB
    
    Setup to load all data as of Sept 29
    
    Single thread Azure function testing
    
    Tweak for Azure function timing test run
    
    Fix database connection string
    
    Fix for email alerts

--- pb_buddy/resources.py
+++ pb_buddy/resources.py
@@ -1,4 +1,8 @@
-{
+alerts = {"alerts": [{"category_num": 2, "alert_name": "Carbon Instincts", "search_string": "price > 3000 and price < 5000 and material.str.contains('Carbon') and ad_title.str.contains('Instinct')", "email_stem": "dustin.brown.andrews", "email_domain": "@gmail.com", "last_checked": "2021-09-25 18:15:06.490215-06:00"}, {"category_num": 2, "alert_name": "Cheap Instincts", "search_string": "price < 3000 and ad_title.str.contains('Instinct')", "email_stem": "dustin.brown.andrews", "email_domain": "@gmail.com", "last_checked": "2021-09-25 18:15:06.577534-06:00"}, {"category_num": 2, "alert_name": "Sights Alert", "search_string": "ad_title.str.contains('Sight') and ad_title.str.contains('7.4') ", "email_stem": "bvmcknight", "email_domain": "@icloud.com", "last_checked": "2021-09-25 18:15:06.656201-06:00"}, {
+    "category_num": 2, "alert_name": "Sights Alert Tracker", "search_string": "ad_title.str.contains('Sight') and ad_title.str.contains('7.4') ", "email_stem": "dustin.brown.andrews", "email_domain": "@gmail.com", "last_checked": "2021-09-25 18:15:06.752468-06:00"}, {"category_num": 12, "alert_name": "Ty's Road Disc Wheelsets", "search_string": "price > 200 and price < 1200 and wheel_size.str.contains('700') and ad_title.str.contains('disc') ", "email_stem": "ty.elgin.andrews", "email_domain": "@gmail.com", "last_checked": "2021-09-25 18:15:06.775972-06:00"}]}
+
+
+category_dict = {
     "All Mountain/Enduro Bikes": 2,
     "DH Bikes": 1,
     "XC Bikes": 75,
