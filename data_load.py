437da2314a15f0f284dc09c72f99d5e0dc199c4a
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-09-22 00:01:03 -0600

    Add loader script for mongo

---
+++ data_load.py
@@ -0,0 +1,61 @@
+# %%
+import pymongo
+import certifi
+import pandas as pd
+from tqdm import tqdm
+import os
+import json
+import pb_buddy.scraper as scraper
+import pb_buddy.utils as ut
+import pb_buddy.data_processors as dt
+
+# %%
+from dotenv import load_dotenv
+load_dotenv()
+
+# %%
+mongo_user = os.environ['MONGO_USER']
+mongo_pass = os.environ['MONGO_PASS']
+# Replace the uri string with your MongoDB deployment's connection string.
+conn_str = f"mongodb+srv://{mongo_user}:{mongo_pass}@cluster0.sce8f.mongodb.net/pb-buddy?retryWrites=true&w=majority"
+# set a 5-second connection timeout
+client = pymongo.MongoClient(
+    conn_str, tlsCAFile=certifi.where(),serverSelectionTimeoutMS=5000)
+
+# %%
+db = client['pb-buddy']
+
+with open("category_dict.json","r") as fh:
+    category_dict = json.load(fh)
+
+# %%
+# LOAD ALL DATA!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+base_data_mongo = db.base_data
+
+
+for i in tqdm(list(range(1,101 + 1))):
+    try:
+        data = dt.get_category_base_data(i)
+    except:
+        continue
+
+    if len(data) == 0:
+        continue
+
+    data = (
+        data
+        .assign(
+            category_num=lambda x: category_dict[data.category.str.strip().unique()[
+                0]],
+            still_for_sale=lambda x:x.still_for_sale.str.replace('<[^<]+?>', "", regex=True))
+    )
+
+    base_data_mongo.insert_many(data.to_dict(orient="records"))
+# %%
+# CHECKS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+cat2_output = cat2_docs.find(
+    {'category': {"$regex":'.*All Mountain/Enduro Bikes.*'}})
+# %%
+cat2_output_df = pd.DataFrame(list(cat2_output))
+cat2_output_df.shape
+# %%
