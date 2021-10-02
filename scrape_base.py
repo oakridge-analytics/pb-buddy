e221b99e17ad0d7a69c7f67811a34937abb22a42
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-10-01 23:23:50 -0600

    Fix for email alerts

--- scrape_base.py
+++ scrape_base.py
@@ -14,7 +14,7 @@ from pb_buddy.resources import category_dict
 
 # %%
 # Settings -----------------------------------------------------------------
-categories_to_scrape = range(100, 100 + 1)
+categories_to_scrape = range(1, 101 + 1)
 num_jobs = os.cpu_count()  # Curently only for initial link grab
 delay_s = 0.0
 log_level = "INFO"
