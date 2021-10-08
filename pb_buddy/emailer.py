9e92f5aa05cd2742c16b732a2d72e8ea528d398c
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-10-08 05:59:05 +0000

    Tweak email alerts style for background and table formatting

--- pb_buddy/emailer.py
+++ pb_buddy/emailer.py
@@ -5,6 +5,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import pandas as pd
 import os
+
 load_dotenv()
 
 
@@ -28,38 +29,45 @@ def email_df(df: pd.DataFrame, email_to: str, email_subject: str):
         Subject for email
     """
     # Get sender's user info
-    env_vars = ["TWILIO_USER","TWILIO_PASS","HOTMAIL_ADDRESS"]
+    env_vars = ["TWILIO_USER", "TWILIO_PASS", "HOTMAIL_ADDRESS"]
     for v in env_vars:
         try:
             os.environ[v]
         except KeyError:
             raise KeyError(
-                "Ensure TWILIO_USER,TWILIO_PASS,HOTMAIL_ADDRESS environ variables are set prior to running!")
+                "Ensure TWILIO_USER,TWILIO_PASS,HOTMAIL_ADDRESS environ variables are set prior to running!"
+            )
 
     msg = MIMEMultipart()
-    msg['From'] = os.environ["HOTMAIL_ADDRESS"]
-    msg['To'] = email_to
-    msg['Subject'] = email_subject
+    msg["From"] = os.environ["HOTMAIL_ADDRESS"]
+    msg["To"] = email_to
+    msg["Subject"] = email_subject
 
     # Build, attach table
+    # html = f"""
+    # <html>
+    # <head></head>
+    # <body>
+    #     {df.to_html()}
+    # </body>
+    # </html>
+    # """
+
     html = f"""
-    <html>
-    <head></head>
-    <body>
+    <table border='2' cellpadding='0' cellspacing='0' width='100%' bgcolor='#e37b46' style='background: rgb(227,123,70); background: linear-gradient(315deg, rgba(227,123,70,1) 3%, rgba(198,51,92,1) 44%, rgba(86,50,139,1) 85%);'>
         {df.to_html()}
-    </body>
-    </html>
+    </table>
     """
 
-    part1 = MIMEText(html, 'html')
+    part1 = MIMEText(html, "html")
     msg.attach(part1)
 
-    s = smtplib.SMTP("smtp.sendgrid.net",587)
+    s = smtplib.SMTP("smtp.sendgrid.net", 587)
     # Hostname to send for this command defaults to the fully qualified domain name of the local host.
     s.ehlo()
     s.starttls()  # Puts connection to SMTP server in TLS mode
     s.ehlo()
-    s.login(os.environ["TWILIO_USER"],os.environ["TWILIO_PASS"])
+    s.login(os.environ["TWILIO_USER"], os.environ["TWILIO_PASS"])
     s.sendmail(msg["From"], msg["To"], msg.as_string())
 
     s.quit()
