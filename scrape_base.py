4309db6ddd9aabb6a5802895e62778de182f7b99
Dustin Andrews <dustin.brown.andrews@gmail.com>
2021-10-09 22:05:35 +0000

    Try tracking newly added ads in scrape process

--- scrape_base.py
+++ scrape_base.py
@@ -157,6 +157,8 @@ for category_to_scrape in np.random.choice(
             dt.write_dataset(
                 new_ads.assign(category_num=category_to_scrape), data_type="base"
             )
+            # Add to lookup for future categories, save a db query
+            all_base_data = pd.concat([all_base_data, new_ads], axis=1)
 
     logging.info(f"Adding {len(new_ads)} new ads to base data")
 
