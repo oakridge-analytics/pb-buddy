05db6837f13a081ff0481bba210c4a367c1f1820
Dustin Andrews <dustin.brown.andrews@gmail.com>
2023-08-22 22:52:31 -0600

    Link list building working

--- scripts/scrape_bike_specs.py
+++ scripts/scrape_bike_specs.py
@@ -1,20 +1,31 @@
 import time
 import re
+import uuid
+from pathlib import Path
 
 from playwright.sync_api import Playwright, sync_playwright, expect
 import fire
 import pandas as pd
 import logging
+import unicodedata
 
-BASE_URL = "https://99spokes.com/en-CA"
+BASE_URL = "https://99spokes.com"
+LANG_REGION = "en-CA"
 NO_BIKES_TEXT = "No bikes found :-("
-START_YEAR = 2020
+URL_MODIFIERS = "region=earth"
+START_YEAR = 2010
 
 logging.basicConfig(level=logging.INFO)
 
 
 def normalize_manufacturer_name(manufacturer: str) -> str:
-    return manufacturer.lower().replace(" ", "-")
+    return remove_accents(manufacturer.lower().replace(" ", "").replace("-", ""))
+
+
+def remove_accents(input_str: str) -> str:
+    return (
+        unicodedata.normalize("NFKD", input_str).encode("ASCII", "ignore").decode("utf-8")
+    )
 
 
 def run(playwright: Playwright) -> None:
@@ -44,13 +55,25 @@ def run(playwright: Playwright) -> None:
     browser.close()
 
 
-def scrape_specs(manufacturer: str, base_url: str = BASE_URL) -> None:
+def scrape_manufacturer_specs(
+    manufacturer: str, links_path_out: str, base_url: str = BASE_URL, delay_s: int = 1
+) -> None:
     with sync_playwright() as playwright:
-        get_manufacturer_data("3T", playwright)
+        get_manufacturer_data(
+            manufacturer,
+            links_path_out=links_path_out,
+            playwright_context=playwright,
+            base_url=base_url,
+            delay_s=delay_s,
+        )
 
 
 def get_manufacturer_data(
-    manufacturer: str, playwright_context: Playwright, base_url: str = BASE_URL
+    manufacturer: str,
+    links_path_out: str,
+    playwright_context: Playwright,
+    base_url: str = BASE_URL,
+    delay_s: int = 1,
 ) -> None:
     browser = playwright_context.chromium.launch(headless=False)
     context = browser.new_context()
@@ -61,28 +84,36 @@ def get_manufacturer_data(
     # First get model families per year
     # Then get models per model family
     # Then get specs per model
-    delay_s = 1
-    for year in range(START_YEAR, pd.datetime.now().year + 1):
-        page.goto(f"{base_url}/bikes?makerId={manufacturer_normalized}&year={year}")
-        logging.info(f"Scraping {manufacturer} {year}...")
+    year_delay_s = 2
+    model_links_out = []
+    for year in range(START_YEAR, pd.Timestamp.now().year + 1):
+        page.goto(
+            f"{base_url}/{LANG_REGION}/bikes?{URL_MODIFIERS}&makerId={manufacturer_normalized}&year={year}"
+        )
+        time.sleep(year_delay_s)
+
+        # Check if there are any bikes for this year, if pattern present anywhere on page
+        if page.get_by_text("No bikes found :-(").is_visible():
+            logging.info(f"No bikes found for {manufacturer} {year}...")
+            continue
+
+        logging.info(f"Scraping Manufactuer: {manufacturer} Year: {year}...")
         model_family_pattern = re.compile(rf"{manufacturer}.*")
         model_family_links = get_pattern_links(page, model_family_pattern)
-        specs = []
-        print(model_family_links)
         for model_family_link in model_family_links:
-            logging.info(f"Scraping {model_family_link}...")
+            logging.info(f"Scraping Model Family: {model_family_link}...")
             page.goto(f"{base_url}/{model_family_link}")
             time.sleep(delay_s)
             model_pattern = re.compile(rf"{year} {manufacturer}.*")
             model_links = get_pattern_links(page, model_pattern)
-            # all_links = get_pattern_links(page, re.compile(r".*"))
-            # print(all_links)
-            for model_link in model_links:
-                page.goto(f"{base_url}/{model_link}")
-                # model_specs = get_specs(page)
-                logging.info(f"Getting specs for model {model_link}...")
-                # specs.append(model_specs)
-                # logging.info(specs)
+            model_links_out.extend(model_links)
+
+    model_links_out = [f"{base_url}/{model_link}" for model_link in model_links_out]
+
+    logging.info(f"Writing links to {links_path_out}...")
+    pd.DataFrame({"URL": model_links_out}).to_csv(
+        Path(links_path_out) / f"{uuid.uuid4()}_{manufacturer}.csv", index=False
+    )
 
 
 def get_pattern_links(page, pattern: re.Pattern):
@@ -94,4 +125,4 @@ def get_pattern_links(page, pattern: re.Pattern):
 
 
 if __name__ == "__main__":
-    fire.Fire(scrape_specs)
+    fire.Fire(scrape_manufacturer_specs)
