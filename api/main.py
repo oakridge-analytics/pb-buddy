1d2d738d9ab65147ba75ffd5027816e571ff7e15
Dustin Andrews <dustin.brown.andrews@gmail.com>
2024-04-24 20:31:45 -0600

    Modal inference working, adding private repo install

--- api/main.py
+++ api/main.py
@@ -4,8 +4,7 @@ from typing import List, Optional
 import modal
 import pandas as pd
 from fastapi import FastAPI
-from joblib import load
-from modal import App, Image, asgi_app, enter, method
+from modal import App, Image, enter, web_endpoint
 from pydantic import BaseModel
 
 
@@ -88,6 +87,11 @@ image = (
     .pip_install("fastapi==0.68.0")
     .pip_install("uvicorn==0.14.0")
     .pip_install("boto3")
+    .pip_install_private_repos(
+        "github.com/pb-buddy/pb-buddy@master",
+        git_user="dbandrews",
+        secrets=[modal.Secret.from_name("pb-buddy-github")],
+    )
     .run_function(
         download_assets,
         secrets=[modal.Secret.from_name("aws-s3-secrets")],
@@ -99,59 +103,47 @@ image = (
 @app.cls(
     image=image,
 )
-class AutoGluonModel:
+class AutoGluonModelInference:
     @enter()
     def load(self):
-        self.model = MultiModalPredictor.load(MODEL_FILE)
-        self.pipeline = load(PIPELINE_FILE)
+        import joblib
+        from autogluon.multimodal import MultiModalPredictor
 
-    @method()
+        self.model = MultiModalPredictor.load(MODEL_FILE)
+        self.pipeline = joblib.load(PIPELINE_FILE)
+
     def transform(self, input_data: pd.DataFrame) -> pd.DataFrame:
         return self.pipeline.transform(input_data)
 
-    @method()
     def get_feature_names_out(self) -> List[str]:
         return self.pipeline.get_feature_names_out()
 
-    @method()
-    def predict(self, input_data: pd.DataFrame, **kwargs) -> str:
-        return self.model.predict(input_data, **kwargs)
+    # @method()
+    @web_endpoint(method="POST")
+    def predict(self, ads: List[BikeBuddyAd]) -> BikeBuddyAdPredictions:
+        # Due to sklearn pipeline needing our target column price_cpi_adjusted_CAD
+        # need to ensure it's present for transform() method
+        df_input = pd.DataFrame(data=[dict(single_ad) for single_ad in ads]).assign(price_cpi_adjusted_CAD=0)
 
+        # # TODO: confirm column names needed for pipeline:
+        # required_input_cols = set(
+        #     itertools.chain(
+        #         *[
+        #             step[2]
+        #             for step in estimator_dict["pipeline"].named_steps["preprocess"].transformers_[
+        #                 :-1
+        #             ]
+        #         ]
+        #     )
+        # )
+        df_predict = pd.DataFrame(
+            data=self.pipeline.transform(df_input),
+            columns=self.pipeline.get_feature_names_out(),
+        )
 
-@web_app.post("/text-predict", tags=["text-predictions"])
-async def predict(ads: List[BikeBuddyAd]) -> BikeBuddyAdPredictions:
-    # Due to sklearn pipeline needing our target column price_cpi_adjusted_CAD
-    # need to ensure it's present for transform() method
-    df_input = pd.DataFrame(data=[dict(single_ad) for single_ad in ads]).assign(price_cpi_adjusted_CAD=0)
+        predictions = self.model.predict(df_predict, as_pandas=False).tolist()
+        if isinstance(predictions, float):
+            predictions = [predictions]
+        outputs = BikeBuddyAdPredictions(predictions=predictions)
 
-    # # TODO: confirm column names needed for pipeline:
-    # required_input_cols = set(
-    #     itertools.chain(
-    #         *[
-    #             step[2]
-    #             for step in estimator_dict["pipeline"].named_steps["preprocess"].transformers_[
-    #                 :-1
-    #             ]
-    #         ]
-    #     )
-    # )
-    autogluon = AutoGluonModel()
-    df_predict = pd.DataFrame(
-        data=autogluon.pipeline.transform.remote(df_input),
-        columns=autogluon.pipeline.get_feature_names_out.remote(),
-    )
-
-    predictions = autogluon.model.predict.remote(df_predict, as_pandas=False).tolist()
-    if isinstance(predictions, float):
-        predictions = [predictions]
-    outputs = BikeBuddyAdPredictions(predictions=predictions)
-
-    return outputs
-
-
-@app.function(
-    image=image,
-)
-@asgi_app()
-def fastapi_app():
-    return web_app
+        return outputs
