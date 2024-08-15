bc6166002d7e910ce0bda6765e189e0f8cc6de90
Dustin Andrews <51100082+dbandrews@users.noreply.github.com>
2024-08-14 22:52:49 -0600

    Add region to scrape settings, add another workflow (#33)

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -1,83 +1,49 @@
 # %%
-import pandas as pd
-import numpy as np
-from tqdm import tqdm
-from dotenv import load_dotenv
 
 # Custom code
-import pb_buddy.scraper as scraper
-import pb_buddy.utils as ut
 import pb_buddy.data_processors as dt
-from pb_buddy.resources import category_dict
 
-load_dotenv("/workspaces/pb-buddy/.env")
-
-# %%
 # Clean base data -----------------------------------------------------
 all_data = dt.get_dataset(-1, data_type="base")
 print(all_data.shape)
 
 # %%
-df_testing = all_data.drop(columns=["_id"])
-
-# %%
-# Plot count over time
-(
-    all_data.assign(scrape_day=lambda x: pd.to_datetime(x.original_post_date).dt.date)
-    .query("scrape_day > @pd.to_datetime('01-OCT-2021')")
-    .groupby("scrape_day")
-    .count()[["url"]]
-    .plot()
+all_data.astype({col: "str" for col in all_data.columns}).to_parquet(
+    "base_data.parquet.gzip", engine="pyarrow", compression="gzip"
 )
 
 # %%
-all_data["scrape_rank"] = all_data.groupby("url", as_index=False)["datetime_scraped"].rank("dense", ascending=False)
+# Write each row of "all_data" as a JSON to "data/base_data" folder
+for index, row in all_data.astype({col: "str" for col in all_data.columns}).iterrows():
+    json_data = row.to_json()
+    file_name = f"data/base_data/{index}.json"
+    with open(file_name, "w") as file:
+        file.write(json_data)
+
 
 # %%
-data_remove = all_data.query("scrape_rank >= 2")
-print(data_remove.shape)
 
 
 # %%
-all_data.loc[all_data.url.isin(data_remove.url), ["url", "datetime_scraped", "category"]].sort_values("url")
+import time
 
-# %%
-dt.remove_from_base_data(data_remove, index_col="_id")
+import duckdb
 
-# %%
-# Remove fake changes ----------------------------------------
-changes = dt.get_dataset(-1, data_type="changes")
-
-# %%
-fake_changes = changes.query("old_value.str.strip() == new_value.str.strip()")
-
-# %%
-db = dt.get_mongodb()
-changes_collection = db.change_data
-
-for val in fake_changes["_id"]:
-    changes_collection.delete_one({"_id": val})
-
-# %%
-# Clean sold data -----------------------------------------------------
-all_sold_data = dt.get_dataset(-1, data_type="sold")
-print(all_sold_data.shape)
-
-# %%
-all_sold_data["scrape_rank"] = all_sold_data.groupby("url", as_index=False)["datetime_scraped"].rank(
-    "dense", ascending=False
-)
-
-# %%
-all_sold_data_remove = all_sold_data.query("scrape_rank >= 2")
-print(all_sold_data_remove.shape)
-
-# %%
-index_col = "_id"
-sold_db_driver = dt.get_mongodb()
-sold_db = sold_db_driver.sold_data
-
-for val in all_sold_data_remove[index_col]:
-    sold_db.delete_one({index_col: val})
+# Read parquet file from S3 bucket bike-buddy/data/base_data.parquet.gzip
+# get start time
+start_time = time.time()
+con = duckdb.connect(database=":memory:")
+con.execute("""
+    CREATE SECRET secret2 (
+    TYPE S3,
+    PROVIDER CREDENTIAL_CHAIN
+);
+""")
+con.execute("CREATE TABLE base_data AS SELECT * FROM parquet_scan('s3://bike-buddy/data/base_data.parquet.gzip')")
+# con.execute("CREATE TABLE base_data AS SELECT * FROM 's3://bike-buddy/data/base_data_json/*.json'")
+df = con.execute("SELECT * FROM base_data").fetch_df()
+print(df.head())
+print(df.shape)
+print(f"Time taken to read parquet file: {time.time() - start_time}")
 
 # %%
