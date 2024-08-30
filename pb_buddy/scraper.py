9489ce9666df6e835ff74e106c7a146b78002b72
Dustin Andrews <dustin.brown.andrews@gmail.com>
2024-08-29 20:02:06 -0600

    Single process Playwright scraping working

--- pb_buddy/scraper.py
+++ pb_buddy/scraper.py
@@ -9,28 +9,21 @@ from tenacity import retry, stop_after_attempt, wait_fixed
 
 
 class PlaywrightScraper:
-    _shared_state = {}
     cookies = []
     headers = {
         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
     }
 
-    def __new__(cls, *args, **kwargs):
-        obj = super(PlaywrightScraper, cls).__new__(cls)
-        obj.__dict__ = cls._shared_state
-        return obj
-
     def __init__(self, headless: bool = True):
-        if not hasattr(self, "initialized"):
-            self.page = None
-            self.browser = None
-            self.headless = headless
-            self.initialized = True
+        self.page = None
+        self.browser = None
+        self.headless = headless
+        self.initialized = True
 
     def start_browser(self):
         if self.browser is None:
             self._playwright = sync_playwright().start()
-            self.browser = self._playwright.chromium.launch(headless=self.headless, args=["--single-process"])
+            self.browser = self._playwright.chromium.launch(headless=self.headless)
             self.context = self.browser.new_context()
             self.context.set_extra_http_headers(self.headers)
             self.page = self.context.new_page()
@@ -95,9 +88,7 @@ def request_ad(url: str, playwright_scraper: PlaywrightScraper, delay_s: int = 1
     str
         Page content as a string
     """
-    playwright = PlaywrightScraper()
-    playwright.start_browser()
-    page_content = playwright.get_page_content(url)
+    page_content = playwright_scraper.get_page_content(url)
 
     time.sleep(delay_s)
     return page_content
